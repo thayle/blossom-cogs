df0cc541853bbabc3a49962a74df9b129b707843
Connor Links <42983904+thayle@users.noreply.github.com>
2020-09-01 09:50:01 -0400

    Update poggers.py

--- poggers/poggers.py
+++ poggers/poggers.py
@@ -4,7 +4,7 @@ class Poggers(commands.Cog):
     """My custom cog"""
 
     @commands.command()
-    async def poggers(self, ctx):
+    async def poggers(self, ctx, *):
         """This does stuff!"""
         # Your code will go here
-        await ctx.send("https://cdn.discordapp.com/attachments/656930670374944829/750344767543574639/poggers.mp4")
+        await ctx.send("I can do stuff!")
