de18734b5957cbcbddf396087815a37c67e2fc83
Connor Links <42983904+thayle@users.noreply.github.com>
2020-09-01 09:43:54 -0400

    Update poggers.py

--- poggers/poggers.py
+++ poggers/poggers.py
@@ -1,18 +1,10 @@
-import discord
 from redbot.core import commands
-import aiohttp
-from numbers import Number
-from random import randint
 
-BaseCog = getattr(commands, "Cog", object)
-
-
-class Poggers(BaseCog):
-    """Show something is Poggers!"""
+class Poggers(commands.Cog):
+    """My custom cog"""
 
     @commands.command()
-    @commands.bot_has_permissions(embed_links=True, add_reactions=True)
-    async def poggers(self, ctx, *, entry_number=None, arg):
-        """Show something is poggers"""
-
-        await ctx.send(arg)
+    async def poggers(self, ctx):
+        """This does stuff!"""
+        # Your code will go here
+        await ctx.send("I can do stuff!")
