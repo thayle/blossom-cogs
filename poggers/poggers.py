342f914a1845211ebdf16238214cd86b005546b8
Connor Links <42983904+thayle@users.noreply.github.com>
2020-09-01 09:54:58 -0400

    Update poggers.py

--- poggers/poggers.py
+++ poggers/poggers.py
@@ -7,3 +7,4 @@ class Poggers(commands.Cog):
     async def poggers(self, ctx):
         """Just send poggers"""
         await ctx.send("https://cdn.discordapp.com/attachments/656930670374944829/750344767543574639/poggers.mp4")
+        await bot.delete_message(ctx.message)
