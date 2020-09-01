4e2b15686f2e9e34d33d36a20b7638c4f308c209
Connor Links <42983904+thayle@users.noreply.github.com>
2020-09-01 09:51:53 -0400

    Update poggers.py

--- poggers/poggers.py
+++ poggers/poggers.py
@@ -1,10 +1,9 @@
 from redbot.core import commands
 
 class Poggers(commands.Cog):
-    """My custom cog"""
+    """Poggers!"""
 
     @commands.command()
-    async def poggers(self, ctx, *):
-        """This does stuff!"""
-        # Your code will go here
+    async def poggers(self, ctx):
+        """Just send poggers"""
         await ctx.send("https://cdn.discordapp.com/attachments/656930670374944829/750344767543574639/poggers.mp4")
