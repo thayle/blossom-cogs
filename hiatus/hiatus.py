396c781cd95e81a75a0a64ec421014ba79d9319f
Connor Links <42983904+thayle@users.noreply.github.com>
2020-09-02 14:44:49 -0400

    Update hiatus.py

--- hiatus/hiatus.py
+++ hiatus/hiatus.py
@@ -1,10 +1,10 @@
 from redbot.core import commands
 
-class Poggers(commands.Cog):
+class Hiatus(commands.Cog):
     """ITS REAL DAMN IT"""
 
     @commands.command(pass_context=True)
-    async def poggers(self, ctx):
+    async def hiatus(self, ctx):
         """ITS REAL DAMN IT"""
         await ctx.send("As Riley's faithful companion it is my job to ensure you all know they are on a **REAL** hiatus. There are many doubters but do not trust them, only the word of Blossom is true all others are fake news with an agenda to undermine our great state. Normal activity will resume Tuesday afternoon. Thank you for your patience and fuck sadik for doubting.")
         await ctx.message.delete()
